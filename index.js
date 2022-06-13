51846ecaa4805dac55f342df7ae0ea345430a42b
gerhynes <gkhynes@gmail.com>
2022-06-13 22:04:56 +0100

    Set up sessions with Redis

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
 const {
   PORT,
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
-  MONGO_PORT
+  MONGO_PORT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET
 } = require("./config/config");
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT
+});
+
+redisClient.connect();
 
 const app = express();
 
@@ -28,12 +42,27 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000000
+    }
+  })
+);
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.send("<h1>Hi There!</h1>");
 });
 
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
